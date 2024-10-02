ffe64eafe7ff2844ed304f16e1b93176532cedfd
Tyler van der Hoeven <hi@tyvdh.com>
2024-10-01 21:52:58 -0400

    misc improvements to make your life easier

--- src/routes/api/contract-id/[signer]/+server.ts
+++ src/routes/api/contract-id/[signer]/+server.ts
@@ -2,7 +2,6 @@ import { server } from '$lib/server/passkeyServer';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
-    const contractId = await server.getContractId(params.signer!);
-
-    return new Response(String(contractId));
+    const contractId = await server.getContractId({ keyId: params.signer! });
+    return new Response(contractId);
 };
