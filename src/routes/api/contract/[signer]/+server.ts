83305d53338590375f0b7c3ee192dee8eb6cdcd2
Elliot Voris <elliot@stellar.org>
2025-02-07 15:12:52 -0600

    package updates and such

--- src/routes/api/contract/[signer]/+server.ts
+++ src/routes/api/contract/[signer]/+server.ts
@@ -2,6 +2,6 @@ import { server } from '$lib/server/passkeyServer';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
-    const contractId = await server.getContractId({ keyId: params.signer! });
+    const contractId = await server.getContractId({ keyId: params.signer });
     return new Response(contractId);
 };
